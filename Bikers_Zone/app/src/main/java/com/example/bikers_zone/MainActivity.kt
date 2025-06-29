45fd1af74d74bad839a0f6ef266278e90ba37d28
Abdullah Ibne Mortuza <abdullah.amad-05-04@dipti.com.bd>
2025-06-30 02:24:38 +0600

    Sign In and Sign Up design implemented using fragment

--- Bikers_Zone/app/src/main/java/com/example/bikers_zone/MainActivity.kt
+++ Bikers_Zone/app/src/main/java/com/example/bikers_zone/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.bikers_zone
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainer
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var signInTextBtn:TextView
+    private lateinit var signUpTextBtn:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        signInTextBtn = findViewById(R.id.signInTV)
+        signUpTextBtn = findViewById(R.id.signUpTV)
+
+        supportFragmentManager.beginTransaction().replace(R.id.container,LoginActivity()).commit()
+
+        signInTextBtn.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.container,LoginActivity()).commit()
         }
+        signUpTextBtn.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.container,SignupActivity()).commit()
+        }
+
     }
 }
---
+++ Bikers_Zone/app/src/main/java/com/example/bikers_zone/SignupActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bikers_zone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignupActivity : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup_activity, container, false)
+    }
+
+}
